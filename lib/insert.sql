e5acdef5afe3cb3379acd0e1e56f01835cf7959f
Brian Bissette <bbissette3@yahoo.com>
2023-04-06 08:53:03 -0500

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Mr. Chocolate', 5, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Rowdy', 6, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive) VALUES
+('Tabitha', 7, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Sergeant Brown', 8, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Melissa', 9, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Grinch', 10, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES('Wendy', 11, 'black', 'M', 'rawr', 1);
+
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES(NULL, 12, 'black', 'M', 'rawr', 1);
